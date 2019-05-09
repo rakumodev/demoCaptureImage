4c1ed8855483a5458240be4db0318e08b5118589
JackTheCode <phatnguyen0987654321@gmail.com>
2019-05-09 14:43:13 +0700

    refs #1: Add sources for fearture "Init and manage different environments and configurations for iOS project"

---
+++ demoCaptureImage/demoCaptureImage/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoCaptureImage
+//
+//  Created by phat nguyen on 5/8/19.
+//  Copyright © 2019 phatnt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
